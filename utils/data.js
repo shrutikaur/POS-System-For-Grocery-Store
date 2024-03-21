3d6dbfb214c13b2a4811415454cd9ef9183575da
Manpreet Kaur Bungai <kaurshruti16@gmail.com>
2024-03-21 15:01:51 +0530

    Add files via upload

---
+++ utils/data.js
@@ -0,0 +1,72 @@
+const items = [
+  {
+    name: "Tea",
+    price: 440,
+    category: "Grocery",
+    image:
+      "/images/tea_leaves.jpg",
+  },
+  {
+    name: "Coffee",
+    price: 750,
+    category: "Grocery",
+    image: "/images/coffee_powder.jpg",
+  },
+  {
+    name: "Chicken",
+    price: 250,
+    category: "Non-Veg",
+    image:
+      "/images/chicken.jpg",
+  },
+  {
+    name: "Meat",
+    price: 1000,
+    category: "Non-Veg",
+    image:
+      "/images/meat.jpg",
+  },
+  {
+    name: "Eggs",
+    price: 60,
+    category: "Non-Veg",
+    image: "/images/egg-tray.jpg",
+  },
+  {
+    name: "Sugar",
+    price: 50,
+    category: "Grocery",
+    image:
+      "/images/madhur-sugar-green-pack.png",
+  },
+  {
+    name: "Wheat Flour",
+    price: 64,
+    category: "Grocery",
+    image:
+      "/images/aashirvaad-whole-wheat-atta-1kg.png",
+  },
+  {
+    name: "Sandwich Bread",
+    price: 40,
+    category: "Grocery",
+    image:
+      "/images/Sandwich_Bread.jpg",
+  },
+  {
+    name: "Basmati Rice 5 Kg Pack",
+    price: 200,
+    category: "Grocery",
+    image:
+      "/images/India-Gate-Basmati-Rice-Bag-Super-5kg.jpg",
+  },
+  {
+    name: "Amul Butter 1 Kg",
+    price: 500,
+    category: "Grocery",
+    image:
+      "/images/amul_butter.png",
+  },
+
+];
+module.exports = items;
